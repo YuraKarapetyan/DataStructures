ae6adcaac85e20331475a55cab7ad55ee24e608d
User <User@DESKTOP-EIH65I7>
2026-03-22 18:12:59 +0400

    Добавьте файлы проекта.

---
+++ DataStructures/Program.cs
@@ -0,0 +1,54 @@
+﻿using MyLinkedList;
+using MyBinaryTree;
+
+namespace MainProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+
+
+            MyQueue<int> queue = new MyQueue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+
+
+            // BINARY TREE
+            MyBinaryTree<int> tree = new MyBinaryTree<int>();
+
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+
+            Console.WriteLine("\nTree (InOrder):");
+            foreach (var item in tree)
+            {
+                Console.Write(item + " ");
+            }
+
+            // Remove one element
+            tree.Remove(3);
+
+            Console.WriteLine("\nAfter remove:");
+            foreach (var item in tree)
+            {
+                Console.Write(item + " ");
+            }
+
+
+
+        }
+    }
+}
---
+++ MyLinkedList/MyLinkedListNode.cs
@@ -0,0 +1,11 @@
+﻿namespace MyLinkedList;
+
+public class MyLinkedListNode<T>
+{
+    public T Value { get; set; }
+    public MyLinkedListNode<T> Next { get; set; }
+    public MyLinkedListNode(T value)
+    {
+        Value = value;
+    }
+}
